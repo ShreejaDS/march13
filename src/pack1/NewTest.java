e80dde7df727d048429140c6f23f0e9e8f2ff2c2
shreej15 <shreej158809@gmail.com>
2018-03-13 11:20:05 +0530

    my second commit

---
+++ src/pack1/NewTest.java
@@ -0,0 +1,14 @@
+package pack1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	  WebDriver d=new ChromeDriver();
+	  d.close();
+  }
+}
